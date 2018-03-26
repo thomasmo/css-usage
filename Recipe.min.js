8522f68afe1bfec2c6d763db4f64bd2ded2473b3
Thomas Moore (CHAKRA) <thomasmo@microsoft.com>
2018-03-26 14:05:40 -0700

    Simplify data collection for better scaling

--- Recipe.min.js
+++ Recipe.min.js
@@ -1781,7 +1781,7 @@ void function() { try {
 	
 } catch (ex) { /* do something maybe */ throw ex; } }();
 
-/* 
+/*
     RECIPE: scriptElemStats
     -------------------------------------------------------------
     Author: thomasmo
@@ -1791,17 +1791,37 @@ void function() { try {
 void function() {
     window.CSSUsage.StyleWalker.recipesToRun.push( function scriptElemStats( element, results) {
         if(element.nodeName.toLowerCase() == "script") {
-            results["data"] = results["data"] || [];
+            results["data"] = results["data"] || {
+                total : 0,
+                async : 0,
+                defer : 0,
+                externalSrc : 0,
+                parentElemHead : 0,
+                parentElemBody : 0,
+                parentElemOther : 0
+            };
+
+            var result = results["data"];
+            result.total++;
+
+            if (element.hasAttribute("async")) {
+                result.async++;
+            }
+            if (element.hasAttribute("defer")) {
+                result.defer++;
+            }
+            if (element.hasAttribute("src")) {
+                result.externalSrc++;
+            }
+
             parentElemName = element.parentNode.nodeName.toLowerCase();
-            results["data"].push({
-                async       : element.hasAttribute("async") ? 1 : 0,
-                defer       : element.hasAttribute("defer") ? 1 : 0,
-                external    : element.hasAttribute("src") ? 1 : 0,
-                parentElem  : { 
-                    head    : (parentElemName === "head") ? 1 : 0,
-                    body    : (parentElemName === "body") ? 1 : 0
-                }
-            });
+            if (parentElemName === "head") {
+                result.parentElemHead++;
+            } else if (parentElemName === "body") {
+                result.parentElemBody++;
+            } else {
+                result.parentElemOther++;
+            }
         }
 
         return results;
